719f810c9fdc9adb22289bbfd73be7f2e8ef039c
Brian Duffey <bduff9@gmail.com>
2018-09-07 12:38:21 -0500

    Fixed sorting algo

--- src/sorting.mjs
+++ src/sorting.mjs
@@ -1,6 +1,6 @@
 export const sortPicks = (teamA, teamB) => {
-	const { points: pointsA, picked: pickedA, average: averageA } = teamA;
-	const { points: pointsB, picked: pickedB, average: averageB } = teamB;
+	const { picked: pickedA, average: averageA } = teamA;
+	const { picked: pickedB, average: averageB } = teamB;
 
 	if (pickedA < pickedB) return 1;
 
@@ -9,10 +9,6 @@ export const sortPicks = (teamA, teamB) => {
 	if (averageA < averageB) return 1;
 
 	if (averageA > averageB) return -1;
-
-	if (pointsA < pointsB) return 1;
-
-	if (pointsA > pointsB) return -1;
 
 	return 0;
 };
--- src/sorting.test.mjs
+++ src/sorting.test.mjs
@@ -2,17 +2,8 @@ import { sortPicks } from './sorting';
 
 describe('sortPicks', () => {
 	const obj1 = { points: 12, picked: 4, average: 3 };
-	const obj2 = { points: 0, picked: 0, average: 0 };
-	const obj3 = { points: 12, picked: 0, average: 0 };
-	const obj4 = { points: 12, picked: 4, average: 0 };
-
-	it('higher points returns -1', () => {
-		expect(sortPicks(obj1, obj2)).toEqual(-1);
-	});
-
-	it('lower points returns 1', () => {
-		expect(sortPicks(obj2, obj1)).toEqual(1);
-	});
+	const obj3 = { points: 9, picked: 3, average: 3 };
+	const obj4 = { points: 8, picked: 4, average: 2 };
 
 	it('higher picks returns -1', () => {
 		expect(sortPicks(obj1, obj3)).toEqual(-1);
